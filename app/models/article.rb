1e15b49a122aeea70c0dbcbccafe0a9497a6abd2
Caner <kurucaner@gmail.com>
2021-12-08 15:35:03 -0500

    new blog fix

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
     validates :title, presence: true, length: { minimum: 5 }
     validates :description, presence: true, length: { minimum: 5 }
+    belongs_to :user
 end
